a7c119e43211c5d6157c6308ec00dbc035c4d0d1
Unknown <fasromero@hotmail.com>
2018-09-26 21:32:22 -0500

    NombreTicket
    verios

---
+++ ejemploUno.js
@@ -0,0 +1,14 @@
+function verHora() {
+    
+    const hoy = new Date();
+    const diaActual = hoy.getDay();
+    const dias = ["Domingo","Lunes","Martes","Miercoles ","Jueves","Viernes","Sabado"];
+    console.log(`Hoy es : ${dias[diaActual]}.`);
+    let hora = hoy.getHours();
+    const minutos = hoy.getMinutes();
+    const segundos = hoy.getSeconds();
+    let amPm = (hora >= 12)? "PM":"AM";
+    hora = (amPm=="PM")? hora-12: hora;
+    console.log(`Hora actual: ${hora}:${minutos}:${segundos} ${amPm}`)
+
+}
