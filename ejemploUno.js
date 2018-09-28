d2885eea166075aaa1b290566beb4c3765602ac1
Git_Croossdevil <osdavid1990@hotmail.com>
2018-09-27 21:31:13 -0500

    TK_OscarL
    Cambio de Lunes y Martes

--- ejemploUno.js
+++ ejemploUno.js
@@ -1,14 +1,14 @@
 function verHora() {
-    
+
     const hoy = new Date();
     const diaActual = hoy.getDay();
-    const dias = ["domingo","lunes","martes","miercoles ","jueves","viernes","sabado"];
+    const dias = ["Domingo", "Lunes", "martes", "miercoles ", "jueves", "viernes", "sabado"];
     console.log(`Hoy es : ${dias[diaActual]}.`);
     let hora = hoy.getHours();
     const minutos = hoy.getMinutes();
     const segundos = hoy.getSeconds();
-    let amPm = (hora >= 12)? "PM":"AM";
-    hora = (amPm=="PM")? hora-12: hora;
+    let amPm = (hora >= 12) ? "PM" : "AM";
+    hora = (amPm == "PM") ? hora - 12 : hora;
     console.log(`Hora actual: ${hora}:${minutos}:${segundos} ${amPm}`)
 
 }
